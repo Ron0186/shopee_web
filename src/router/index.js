a6c65af2b832cccfb379e087c562c9025a1815c9
vicc9 <geegle2@gmail.com>
2025-03-24 18:35:15 +0800

    合併

--- src/router/index.js
+++ src/router/index.js
@@ -64,7 +64,6 @@ const router = createRouter({
         },
         // 幫助中心搜尋內容頁面
         {
-<<<<<<< HEAD
           path: "/search",
           name: "search-link",
           component: SearchResult,
@@ -75,18 +74,6 @@ const router = createRouter({
           name: "article-link",
           component: HelpCenter,
           props: true,
-=======
-          path: '/search',
-          name: 'search-link',
-          component: () => import("@/components/SearchResult.vue"),
-          props: route => ({ query: route.query.q })
-        },
-        {
-          path: '/article/:id',
-          name: 'article-link',
-          component: () => import("@/views/pages/HelpCenter.vue"),
-          props: true
->>>>>>> origin/Arthur
         },
         //使用者登入頁
         {
@@ -121,15 +108,6 @@ const router = createRouter({
           path: "/shop/apply",
           name: "ApplyShop",
           component: () => import("@/views/front/ApplyShop.vue"),
-<<<<<<< HEAD
-=======
-        },
-        //賣家商店頁
-        {
-          path: "/store/:shopId",
-          name: "Store",
-          component: () => import("@/views/front/SellerStore.vue"),
->>>>>>> origin/Arthur
         },
 
         //客服中心
