e01aea146eea787e453a5518e54898b9fefd3234
chien <cyhsu1019@gmail.com>
2025-03-30 16:37:39 +0800

    存

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,14 +10,28 @@
 
     <!-- 會員中心按鈕 -->
     <div class="nav-icons">
-      <router-link to="/user/login" v-if="!userStore.username">🔑
-        登入</router-link>
-      <router-link to="/user/register" v-if="!userStore.username">📝
-        註冊</router-link>
-      <router-link to="/shop/apply" v-if="userStore.token && !userStore.roles.includes('SELLER')">📝
-        我要當賣家!!</router-link>
+      <router-link to="/user/login" v-if="!userStore.username"
+        >🔑 登入</router-link
+      >
+      <router-link to="/user/register" v-if="!userStore.username"
+        >📝 註冊</router-link
+      >
+      <router-link
+        to="/shop/apply"
+        v-if="userStore.token && !userStore.roles.includes('SELLER')"
+        >📝 我要當賣家!!</router-link
+      >
+      <router-link
+        :to="'/store/' + userStore.shopId"
+        v-if="userStore.token && userStore.roles.includes('SELLER')"
+      >
+        🏪 賣家中心
+      </router-link>
       <router-link to="/profile">👤 會員中心</router-link>
-      <router-link v-if="userStore.username" :to="userStore.isSeller ? '/seller/orders' : '/user/orders'">
+      <router-link
+        v-if="userStore.username"
+        :to="userStore.isSeller ? '/seller/orders' : '/user/orders'"
+      >
         📦 訂單管理
       </router-link>
       <router-link to="/cart">🛒 購物車</router-link>
@@ -36,61 +50,70 @@
         <button @click="toggleCategory">🛍 商城分類 ▼</button>
         <ul v-if="categoryOpen">
           <li>
-            <router-link to="/shop?category=clothing" @click="toggleDrawer">👕
-              衣服</router-link>
+            <router-link to="/shop?category=clothing" @click="toggleDrawer"
+              >👕 衣服</router-link
+            >
           </li>
           <li>
-            <router-link to="/shop?category=electronics" @click="toggleDrawer">📱 電子產品</router-link>
+            <router-link to="/shop?category=electronics" @click="toggleDrawer"
+              >📱 電子產品</router-link
+            >
           </li>
           <li>
-            <router-link to="/shop?category=home" @click="toggleDrawer">🏠
-              家用品</router-link>
+            <router-link to="/shop?category=home" @click="toggleDrawer"
+              >🏠 家用品</router-link
+            >
           </li>
           <li>
-            <router-link to="/shop?category=others" @click="toggleDrawer">🔹
-              其他</router-link>
+            <router-link to="/shop?category=others" @click="toggleDrawer"
+              >🔹 其他</router-link
+            >
           </li>
         </ul>
       </li>
       <li>
-        <router-link to="/discounts" @click="toggleDrawer">💰 優惠專區</router-link>
+        <router-link to="/discounts" @click="toggleDrawer"
+          >💰 優惠專區</router-link
+        >
       </li>
       <li>
-        <router-link to="/notifications" @click="toggleDrawer">🔔
-          通知</router-link>
+        <router-link to="/notifications" @click="toggleDrawer"
+          >🔔 通知</router-link
+        >
       </li>
       <li>
-        <router-link to="/customerService" @click="toggleDrawer">📞 客服 &
-          幫助中心</router-link>
+        <router-link to="/customerService" @click="toggleDrawer"
+          >📞 客服 & 幫助中心</router-link
+        >
       </li>
       <li>
-        <router-link to="/address" @click="toggleDrawer">📍 地址管理</router-link>
+        <router-link to="/address" @click="toggleDrawer"
+          >📍 地址管理</router-link
+        >
       </li>
       <li>
-        <router-link to="/payment-methods" @click="toggleDrawer">💳
-          付款方式</router-link>
+        <router-link to="/payment-methods" @click="toggleDrawer"
+          >💳 付款方式</router-link
+        >
       </li>
       <li>
-        <router-link to="/privacy" @click="toggleDrawer">📜 隱私政策 &
-          使用者條款</router-link>
+        <router-link to="/privacy" @click="toggleDrawer"
+          >📜 隱私政策 & 使用者條款</router-link
+        >
       </li>
     </ul>
   </div>
 </template>
 
 <script setup>
-import { ref, } from "vue";
-import { useUserStore } from '@/stores/user';
+import { ref } from "vue";
+import { useUserStore } from "@/stores/user";
 import Swal from "sweetalert2";
 import router from "@/router/index";
 
 const userStore = useUserStore();
 const drawerOpen = ref(false);
 const categoryOpen = ref(false);
-
-
-
-
 
 const toggleDrawer = () => {
   drawerOpen.value = !drawerOpen.value;
@@ -109,7 +132,7 @@ async function logout() {
     confirmButtonText: "OK",
   });
   if (response.isConfirmed) {
-    router.push('/shop');
+    router.push("/shop");
   }
 }
 </script>
@@ -130,7 +153,7 @@ async function logout() {
   box-sizing: border-box;
 }
 
-.title>a {
+.title > a {
   color: #000;
   text-decoration: none;
   font-weight: 600;
@@ -222,7 +245,7 @@ async function logout() {
 .logout-link {
   color: #000;
   text-decoration: none;
-  font-size: 16px
+  font-size: 16px;
 }
 
 .logout-link:hover {
