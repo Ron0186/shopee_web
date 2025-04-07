3ca8913194e5dca57e918de6c48b2f586786e16b
Arthur <arthur52105210@gmail.com>
2025-04-08 01:14:02 +0800

    navbar新增 連結到/shop & /shop/list

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,6 +10,16 @@
 
     <!-- 會員中心按鈕 -->
     <div class="nav-icons">
+
+      <router-link to="/shop" class="nav-link">
+        <span class="nav-icon">👕</span>
+        <span class="nav-text">所有商品</span>
+      </router-link>
+      
+      <router-link :to="{ name: 'ShopListPage' }" class="nav-link">
+        <span class="nav-icon">🏬</span>
+        <span class="nav-text">所有商店</span>
+      </router-link>
       <!-- 主要導航按鈕 - 精簡了導航選項 -->
       <router-link to="/cart" class="nav-link">
         <span class="nav-icon">🛒</span>
